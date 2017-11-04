02e59f2b4f79b6b5b253d0856e4d4ac664fd280b
Sudhir Voleti <sudhir_voleti@isb.edu>
2017-11-04 15:59:02 +0530

    Update reticulated py funcs.R

--- reticulated py funcs.R
+++ reticulated py funcs.R
@@ -174,3 +174,70 @@ extract_phrases <- function(corpus_df, noun=TRUE){   # output from py.annotate()
   {phrase_list = lapply(corpus_split_list, function(x) {phrase_detector(x)})}  
   
   return(phrase_list)    }   # extract_phrases() func ends
+
+      
+### +++ Build [person | Org | GPE] entity detector (& later, extractor) func
+
+## func to do doc-wise entity collection          
+entity_list_doc <- function(doc_df){ 
+  
+  # code exceptions for insufficient df rows
+  if (nrow(doc_df) == 0) {b0 = NULL}
+  if (nrow(doc_df) < 3) {
+    b0 = paste0(doc_df$token, collapse = " ")
+  } else {
+    
+    index = 1  
+    for (i1 in 2:nrow(doc_df)){
+      
+      # not needed now since entity_type = "PERSON" is prespecified
+      # a01 = (doc_df$entity_type[i1] == doc_df$entity_type[i1-1]) 
+      
+      a02 = (doc_df$chunk_type[i1-1] == "I")*(doc_df$chunk_type[i1] == "B")
+      if (!a02) {doc_df$index_col[i1] = index; 
+      doc_df$index_col[i1-1] = index} else 
+      { index = index + 1;
+      doc_df$index_col[i1] = index}
+    } # i1 loop ends
+    
+    b0 = sapply(seq(1:max(doc_df$index_col)),
+                function(x) {
+                  paste0(doc_df$token[(doc_df$index_col == x)], collapse = " ") })
+  } # else ends
+  
+  return(b0)    }    # entity_list_doc() func ends
+
+      
+## build wrapper func around entity_list_doc() entity extractor  
+extract_entity <- function(corpus, 
+                           entity = "PERSON"){    # Alternately, "ORGANIZATION", "GPE"  
+  
+  require(tidyverse)
+  
+  # retain only relevant rows w.r.t. named_entity field
+  match_term1 = paste0("B-", entity)
+  match_term2 = paste0("I-", entity)
+  logi_vec = (text_df$named_entity == match_term1 | text_df$named_entity == match_term2)
+  text_df_subsetted = text_df[logi_vec,]
+  head(text_df_subsetted, 10)
+  
+  # narrowing analysis df to only relevant colms
+  selected_df = text_df_subsetted %>% 
+    select(token, named_entity, doc_num) %>%    # first, keep relevant colms
+    mutate(serial_num = row_number()) %>%     # build a serial_num colm
+    mutate(index_col = 0) %>%
+    separate(named_entity, c("chunk_type", "entity_type")) # %>% head()
+  head(selected_df)
+  
+  docs = unique(selected_df$doc_num)
+  
+  outp_list = lapply(docs, 
+                     function(x){entity_list_doc(selected_df[(selected_df$doc_num == x),])})
+  
+  # doc_df = selected_df[(selected_df$doc_num == 1),]
+  
+  return(outp_list)    }    # extract_entity() func ends
+
+# example is below, commented out based on ibm corpus
+# text_df = py.annotate(ibm_corpus, ner = TRUE); ibm_orgs_list = extract_entity(text_df, entity = "ORGANIZATION") 
+###      
